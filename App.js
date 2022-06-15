7c8e2452e141df5f0cdd797feef5a8719bfa6ca9
coder-sys <71105356+coder-sys@users.noreply.github.com>
2022-06-14 19:29:20 -0700

    Update App.js

--- App.js
+++ App.js
@@ -12,17 +12,6 @@ import HomePage from './homepage'
 import FolderContent from './foldercontent';
 import Folder from './folder';
 import Frontpage from './frontpage';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, } from 'three/examples/jsm/controls/OrbitControls';
-import { BoxBufferGeometry, MeshLambertMaterial } from 'three';
-function Box(){
-  return (
-    <mesh>
-      <boxBufferGeometry attach={'geometry'} />
-      <MeshLambertMaterial  attach={'material'} color={'hotpink'}/>
-    </mesh>
-  )
-}
 export default function App() {
   return (
     <SafeAreaProvider style={styles.container}>
